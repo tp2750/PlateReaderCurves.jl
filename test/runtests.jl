98abf98fdbba7581ec77425edc6ae2f24f8fde64
Thomas A. Poulsen <thomas@lha66.dk>
2020-12-06 21:26:17 +0100

    disable plot test for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using PlateReaderCurves
 using Test
-using Plots
+# using Plots
 
 @testset "ReaderCurves.jl" begin
     s1 = collect(0:10:100)
@@ -12,6 +12,6 @@ using Plots
                       value_unit = "OD405nm",
                       )
     @test A01.reader_value[11] == 0.05 + 4 * (1 - exp(-1))
-    plot(A01)
+    # plot(A01)
 end
 
