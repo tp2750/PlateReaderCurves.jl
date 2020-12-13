d6f2b9e903b1569211c0f6b3b7c2f68e570fecf1
Thomas A. Poulsen <thomas@lha66.dk>
2020-12-13 08:39:58 +0100

    datastructure for fit

--- src/ReaderCurves.jl
+++ src/ReaderCurves.jl
@@ -11,11 +11,37 @@
 """
 Base.@kwdef struct ReaderCurve
     well_name::String = "well"
-    kinetic_time::Array
+    kinetic_time::Array{Real}
     reader_value::Array{Union{Missing, Real}}
     reader_temperature::Array{Union{Missing, Real}} = [missing]
     time_unit::String
     value_unit::String
     temperature_unit::String = "C"
 end
+
+"""
+    ReaderCurveFit: Datastructure for holding reader curves and corresponding fits
+    Fields:
+    readercurve::ReaderCurve the input readercurve
+    fit_method::String name of method to fit (linreg_trim, )
+    fit_input_parameters::NamedTuple parameters given to fit method
+    predict::Function fitted function. Can be used to predict new fitted values
+    slope::Real max slope
+    intercept::Real intercept of max slope curve
+    fit_mean_residual::Real average absolute residuals of fit and read
+"""
+Base.@kwdef struct ReaderCurveFit
+    readercurve::ReaderCurve
+    fit_method::String
+    fit_input_parameters::NamedTuple
+    predict::Function
+    slope::Real
+    intercept::Real
+    fit_mean_residual::Real
+end
+
+
+
+
+
 
