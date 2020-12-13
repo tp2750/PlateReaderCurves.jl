0873ded2ecd2d7cfb508400cb59a97dbe9b2c07b
Thomas A. Poulsen <thomas@lha66.dk>
2020-12-13 08:38:37 +0100

    plot readercurve with fit

--- src/plots.jl
+++ src/plots.jl
@@ -5,9 +5,10 @@
 @recipe function f(r::ReaderCurve; markersize=6)
     seriestype := :scatter
     label --> ""
-    marker --> :+
+    marker --> :circle
     markersize --> markersize
-    markercolor --> "black"
+    markerstroke --> 2
+    markercolor --> :black
     title --> r.well_name
     xguide --> r.time_unit
     yguide --> r.value_unit
@@ -15,6 +16,44 @@
     ## size --> (200,200)
     ## titlefontsize --> 8
     r.kinetic_time, r.reader_value
+end
+
+
+
+
+"""
+    Plot a readercurve-fit
+    plot(::ReaderCurveFit; marker_size=6)    
+"""
+@recipe function f(r::ReaderCurveFit; markersize=6, parameters=true)
+    title --> r.readercurve.well_name
+    if parameters
+        @series begin
+            label --> ""
+            series_annotations := ["α = $(round(r.slope, digits=3))", "y0 = $(round(r.intercept,digits=3))"]
+            x0 = r.readercurve.kinetic_time[1]
+            y0 = maximum(r.readercurve.reader_value)
+            [x0,x0], [y0,y0*.9]
+        end
+    end
+    @series begin
+        r.readercurve
+    end
+    
+    @series begin
+        seriestype := :scatter
+        label --> ""
+        markershape --> :xcross
+        markercolor --> :grey
+        r.readercurve.kinetic_time, r.predict(r.readercurve.kinetic_time)
+    end
+
+    @series begin
+        seriestype := :line
+        label --> ""
+        seriescolor --> :blue
+        r.readercurve.kinetic_time, r.intercept .+ (r.slope .* r.readercurve.kinetic_time)
+    end
 end
 
 
