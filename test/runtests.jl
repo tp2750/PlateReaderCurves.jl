c8cbcbe87fd48ea858b3df3cb4c2d3c94a38e94e
Thomas A. Poulsen <thomas@lha66.dk>
2020-12-15 20:47:14 +0100

    test plate contructor

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,6 +20,37 @@ using DataFrames, CSV
         A01_fit3 = fit(A01,"smooth_spline"; lambda = 250)
         @test A01_fit3.predict.([0,1]) == [0.05655762222793344, 0.09394291903679455]
     end
+    @testset "fit with missing" begin
+        s1 = collect(0:10:100)
+        A02 = ReaderCurve(well_name = "A02",
+                          kinetic_time = s1,
+                          reader_value = replace( 2 .* s1, 60 => NaN),
+                          time_unit = "sec",
+                          value_unit = "OD405nm",
+                          )
+        A03 = ReaderCurve(well_name = "A03",
+                          kinetic_time = s1,
+                          reader_value = replace( 2 .* s1, 60 => missing),
+                          time_unit = "sec",
+                          value_unit = "OD405nm",
+                          )
+        A04 = ReaderCurve(well_name = "A04",
+                          kinetic_time = s1,
+                          reader_value = replace( 2 .* s1, 60 => Inf),
+                          time_unit = "sec",
+                          value_unit = "OD405nm",
+                          )
+        # A02_fit1 = fit(A02,"linreg_trim")
+        # A02_fit2 = fit(A02,"max_slope")
+        # A02_fit3 = fit(A02,"smooth_spline")
+        # A03_fit1 = fit(A03,"linreg_trim")
+        # A03_fit2 = fit(A03,"max_slope")
+        # A03_fit3 = fit(A03,"smooth_spline")
+        # A04_fit1 = fit(A04,"linreg_trim")
+        # A04_fit2 = fit(A04,"max_slope")
+        # A04_fit3 = fit(A04,"smooth_spline")
+ 
+    end    
 end
 @testset "Bubble" begin
     B01_df = CSV.File("b01.csv") |> DataFrame
@@ -36,4 +67,34 @@ end
     B01_fit3 = fit(B01,"smooth_spline"; lambda = 250)
     @test B01_fit3.predict.([0,1]) == [0.08762157518257128, 0.08767650822148132]
 end
-
+@testset "Plate" begin
+    s1 = collect(0:10:100)
+    y1 = PlateReaderCurves.rc_exp(s1, 4, 100, 0.05) ## 0.05 .+ 4 .* (1 .- exp.(-s1 ./100)),
+    A01 = ReaderCurve(well_name = "A01",
+                      kinetic_time = s1,
+                      reader_value = y1,
+                      time_unit = "sec",
+                      value_unit = "OD405nm",
+                      )
+    A02 = ReaderCurve(well_name = "A01",
+                      kinetic_time = s1,
+                      reader_value = y1 .+ 0.1,
+                      time_unit = "sec",
+                      value_unit = "OD405nm",
+                      )
+    A03 = ReaderCurve(well_name = "A01",
+                      kinetic_time = s1,
+                      reader_value = y1 .- 0.1,
+                      time_unit = "sec",
+                      value_unit = "OD405nm",
+                      )
+    Plate_1 = ReaderPlate(
+        readerplate_id = "1117389a-9abe-4cf9-9feb-ec7fa1aa0933",
+        readerplate_barcode = "",
+        readerfile_name = "testFile",
+        readerplate_number = 1,
+        readerplate_geometry = 96,
+        readercurves = [A01, A02, A03]
+    )
+    @test length(Plate_1) == 3
+end
