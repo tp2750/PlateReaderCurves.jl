0c24b0b38f00e06898cb99039f798cafed2d3458
Thomas A. Poulsen <thomas@lha66.dk>
2021-01-03 14:09:33 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using PlateReaderCurves
 using Test
-using DataFrames, CSV
+using DataFrames, CSV, DataFramesMeta
 
 @testset "ReaderCurves.jl" begin
     s1 = collect(0:10:100)
@@ -160,9 +160,11 @@ end
     @test length(dat1) == 1
     @test length(dat1.readerplates[1]) == geometry(dat1)
     dat1_fit1 = rc_fit(dat1, "linreg_trim")
+    ## Subset on a quadrant
     dat1_fit1_Q3 = Q(dat1_fit1.readerplates[1], "Q3")
-    dat1_fit1_Q3.readerplates[1].readercurve
     @test  map(x -> x.readercurve.readerplate_well, dat1_fit1_Q3.readercurves) == ["B01", "B03", "B05", "B07", "B09", "B11", "B13", "B15", "B17", "B19", "B21", "B23", "D01", "D03", "D05", "D07", "D09", "D11", "D13", "D15", "D17", "D19", "D21", "D23", "F01", "F03", "F05", "F07", "F09", "F11", "F13", "F15", "F17", "F19", "F21", "F23", "H01", "H03", "H05", "H07", "H09", "H11", "H13", "H15", "H17", "H19", "H21", "H23", "J01", "J03", "J05", "J07", "J09", "J11", "J13", "J15", "J17", "J19", "J21", "J23", "L01", "L03", "L05", "L07", "L09", "L11", "L13", "L15", "L17", "L19", "L21", "L23", "N01", "N03", "N05", "N07", "N09", "N11", "N13", "N15", "N17", "N19", "N21", "N23", "P01", "P03", "P05", "P07", "P09", "P11", "P13", "P15", "P17", "P19", "P21", "P23"]
+    @test dat1_fit1_Q3.readerplate_geometry == 384
     dat1_fit1_Q3_96 = Q(dat1_fit1.readerplates[1], "Q3"; well96 = true)
     @test map(x -> x.readercurve.readerplate_well, dat1_fit1_Q3_96.readercurves) == ["A01", "A02", "A03", "A04", "A05", "A06", "A07", "A08", "A09", "A10", "A11", "A12", "B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B09", "B10", "B11", "B12", "C01", "C02", "C03", "C04", "C05", "C06", "C07", "C08", "C09", "C10", "C11", "C12", "D01", "D02", "D03", "D04", "D05", "D06", "D07", "D08", "D09", "D10", "D11", "D12", "E01", "E02", "E03", "E04", "E05", "E06", "E07", "E08", "E09", "E10", "E11", "E12", "F01", "F02", "F03", "F04", "F05", "F06", "F07", "F08", "F09", "F10", "F11", "F12", "G01", "G02", "G03", "G04", "G05", "G06", "G07", "G08", "G09", "G10", "G11", "G12", "H01", "H02", "H03", "H04", "H05", "H06", "H07", "H08", "H09", "H10", "H11", "H12"]
+    @test dat1_fit1_Q3_96.readerplate_geometry == 96
 end
